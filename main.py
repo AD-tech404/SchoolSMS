7116c582587dbf87b7c222b75392a2a8bdb69aba
AD-tech404 <dileepjaiswal404@gmail.com>
2025-10-03 10:47:22 +0000

    All done
    Done upto Academic pages of classes.

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for
+from classes.class1.class1A import class1a_bp
+
+app = Flask(__name__)
+
+app.secret_key = 'ad'  # REQUIRED for flashing messages
+
+app.register_blueprint(class1a_bp)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    class_name = request.form.get('class_name')
+    if class_name == 'class1':
+        return redirect(url_for('class1a.class1a_home'))
+    else:
+        return "Unknown module selected"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
